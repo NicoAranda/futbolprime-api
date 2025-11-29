b31e67c3119b4f024167eff7ed9b9cd87d3d0214
fernaaandaaaa <168448958+fernaaandaaaa@users.noreply.github.com>
2025-11-28 22:18:37 -0300

    swagger  crear lista deseos item dto

--- src/main/java/com/futbolprime/futbolprime_api/dto/listaDeseos/CrearListaDeseosItemDTO.java
+++ src/main/java/com/futbolprime/futbolprime_api/dto/listaDeseos/CrearListaDeseosItemDTO.java
@@ -1,9 +1,26 @@
 package com.futbolprime.futbolprime_api.dto.listaDeseos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "CrearListaDeseosItemDTO",
+        description = "DTO utilizado para agregar un producto a la lista de deseos de un usuario."
+)
 public class CrearListaDeseosItemDTO {
+
+    @Schema(
+            description = "ID del usuario dueño de la lista de deseos",
+            example = "12",
+            required = true
+    )
     private Long usuarioId;
+
+    @Schema(
+            description = "ID del producto que se desea agregar a la lista de deseos",
+            example = "55",
+            required = true
+    )
     private Long productoId;
 }
