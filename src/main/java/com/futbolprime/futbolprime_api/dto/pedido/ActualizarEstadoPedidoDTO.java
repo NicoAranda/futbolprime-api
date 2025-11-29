ef0bf1d5c3c954ead49260c7faf613e649c61d67
fernaaandaaaa <168448958+fernaaandaaaa@users.noreply.github.com>
2025-11-28 22:31:41 -0300

    swagger actualizar estado pedido dto

--- src/main/java/com/futbolprime/futbolprime_api/dto/pedido/ActualizarEstadoPedidoDTO.java
+++ src/main/java/com/futbolprime/futbolprime_api/dto/pedido/ActualizarEstadoPedidoDTO.java
@@ -1,8 +1,19 @@
 package com.futbolprime.futbolprime_api.dto.pedido;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "ActualizarEstadoPedidoDTO",
+        description = "DTO utilizado para actualizar el estado de un pedido."
+)
 public class ActualizarEstadoPedidoDTO {
+
+    @Schema(
+            description = "Nuevo estado del pedido. Puede ser: PENDIENTE, PAGADO, ENVIADO, COMPLETADO, CANCELADO.",
+            example = "ENVIADO",
+            required = true
+    )
     private String estado;
 }
