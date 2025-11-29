adf789b6aa877be252efc893b4de8b0b090971fe
fernaaandaaaa <168448958+fernaaandaaaa@users.noreply.github.com>
2025-11-28 22:22:13 -0300

    swagger  lista deseos item dto

--- src/main/java/com/futbolprime/futbolprime_api/dto/listaDeseos/ListaDeseosItemDTO.java
+++ src/main/java/com/futbolprime/futbolprime_api/dto/listaDeseos/ListaDeseosItemDTO.java
@@ -1,12 +1,25 @@
 package com.futbolprime.futbolprime_api.dto.listaDeseos;
 
 import com.futbolprime.futbolprime_api.dto.producto.ProductoDTO;
-import com.futbolprime.futbolprime_api.model.Producto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "ListaDeseosItemDTO",
+        description = "Representa un producto dentro de la lista de deseos del usuario."
+)
 public class ListaDeseosItemDTO {
 
+    @Schema(
+            description = "ID único del ítem dentro de la lista de deseos",
+            example = "21"
+    )
     private Long id;
+
+    @Schema(
+            description = "Datos del producto asociado al ítem",
+            implementation = ProductoDTO.class
+    )
     private ProductoDTO producto;
 }
