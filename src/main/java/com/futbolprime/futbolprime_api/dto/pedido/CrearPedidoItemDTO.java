2295c74ba2d7aede9a0890dfd29df2f261be5e2a
fernaaandaaaa <168448958+fernaaandaaaa@users.noreply.github.com>
2025-11-28 22:33:20 -0300

    swagger crear pedido item dto

--- src/main/java/com/futbolprime/futbolprime_api/dto/pedido/CrearPedidoItemDTO.java
+++ src/main/java/com/futbolprime/futbolprime_api/dto/pedido/CrearPedidoItemDTO.java
@@ -1,9 +1,26 @@
 package com.futbolprime.futbolprime_api.dto.pedido;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "CrearPedidoItemDTO",
+        description = "DTO que representa un ítem dentro del pedido, indicando producto y cantidad solicitada."
+)
 public class CrearPedidoItemDTO {
+
+    @Schema(
+            description = "ID del producto que se está agregando al pedido",
+            example = "45",
+            required = true
+    )
     private Long productoId;
+
+    @Schema(
+            description = "Cantidad solicitada del producto",
+            example = "2",
+            required = true
+    )
     private Integer cantidad;
 }
